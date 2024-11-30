63159b8a206c02841fa911e9960aa35057f55ce7
sadeval <sadeval2011@gmail.com>
2024-11-30 16:32:10 +0200

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Registration.Models;
+
+namespace Registration.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] UserRegistration model)
+        {
+            Console.WriteLine($"Username: {model.Username}, Email: {model.Email}, Password: {model.Password}");
+
+            if (string.IsNullOrEmpty(model.Username) || string.IsNullOrEmpty(model.Email) || string.IsNullOrEmpty(model.Password))
+            {
+                return Json(new { success = false, message = "All fields are required." });
+            }
+
+            return Json(new { success = true, message = "Registration successful!" });
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
